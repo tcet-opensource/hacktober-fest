3e488f177d7665b17f4d0d4033b78fd8da07f07e
kamalika0363 <59131714+doesnots@users.noreply.github.com>
2023-09-07 22:08:18 +0530

    Hacktoberfest 2023

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Example from "./components/Example/Example";
+function App() {
+  return (
+    <div className="">
+      <Example />
+    </div>
+  );
+}
+
+export default App;
