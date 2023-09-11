c903d3e21975a68636966a255290fb9aa559216c
pranaytiwariii <pranaytiwariprpk@gmail.com>
2023-09-11 21:34:17 +0530

    [FIXED]close icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import logo from "/Navbar/logo.png";
 import menu from "/Navbar/menu.svg";
-import close from "/Navbar/close.svg";
 import discord from "/Navbar/discord.svg";
+import close1 from "/Navbar/close1.svg";
 
 export default function Navbar() {
   const [toggle, setToggle] = useState(false);
@@ -37,19 +37,19 @@ export default function Navbar() {
     },
   ];
   return (
-    <div className="px-6 pt-4 pb-2 flex justify-between text-white">
+    <div className="px-6 pt-4 pb-2 flex justify-between text-[#AFAFAF]">
       <div className="flex items-center">
         <img src={logo} className="h-8 w-8" alt="" />
-        <p className="text-sm ">TCET OpenSource</p>
+        <p className="text-lg font-medium text-white">TCET OpenSource</p>
       </div>
       {/* ----------full screen---------- */}
       <div className="flex">
-        <ul className="hidden lg:flex lg:gap-6 text-sm items-center">
+        <ul className="hidden lg:flex lg:gap-6 text-md items-center">
           {navLink.map((step) => (
             <div key={step}>
               <figure>
                 <li>
-                  <a className="hover:underline-offset-8" href={step.link}>
+                  <a className="hover:underline-offset-8 hover:text-white" href={step.link}>
                     {step.header}
                   </a>
                 </li>
@@ -65,18 +65,18 @@ export default function Navbar() {
             <a href="">Discord</a>
           </div>
         </button>
-        <button className="text-lg  px-4 py-2 rounded-lg mt-0 bg-indigo-500">
+        <button className="text-lg px-4 py-2 rounded-lg mt-0 bg-[#4C58FE] ">
           <a href="">Register Now</a>
         </button>
       </div>
       {/* ----------tablet---------- */}
       <div className="z-30 lg:hidden items-center" onClick={handleClick}>
-        <img className="h-8 w-8" src={toggle ? close : menu} alt="" />
+        <img className="h-8 w-8" src={toggle ? close1 : menu} alt="" />
       </div>
       <ul
         className={
           toggle
-            ? "navLinks absolute flex flex-col gap-4 text-lg w-full px-8 mt-8 lg:hidden"
+            ? "navLinks absolute flex flex-col gap-4 text-md w-full px-8 mt-12 lg:hidden"
             : "hidden"
         }
       >
