b90daf6af170e8190324fa3c434013d5ac82b681
Horror26 <1032210438@tcetmumbai.in>
2023-09-14 00:30:27 +0530

    testing component[FAQ] section deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import FAQSection from "./components/FAQSection/FAQSection";
 function App() {
   // do not add css in this file
   return (
     <div className="">
       <Navbar />
-      <FAQSection />
     </div>
   );
 }
