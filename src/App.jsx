5c9e024b6cb620ab18cd7d2a4eeafbd4486a9a6c
kamalika0363 <59131714+doesnots@users.noreply.github.com>
2023-09-15 00:29:31 +0530

    04-explore-section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@
 import Navbar from "./components/Navbar/Navbar";
 import FAQSection from "./components/FAQSection/FAQSection";
 import SponsorSection from "./components/SponsorSection/SponsorSection";
-// import Explore from "./components/exploreSection/explore";
+import Explore from "./components/exploreSection/explore";
 
 function App() {
   // do not add css in this file
   return (
     <div className="">
       <Navbar />
-      {/*<Explore />*/}
+      <Explore />
       <SponsorSection />
       <FAQSection />
     </div>
