5dd1a3757db8444b5016ed291b1d5c85d01524da
Shivam Mishra <shivam.r.mishra9120@gmail.com>
2023-09-15 21:47:47 +0530

    [UPDATED] Fetch function updated to store data fetched from api for a session

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-explore': 'linear-gradient(108deg, #161136 5.21%, #0e0b23 98.76%)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
