8f646de280ec1d4227561e46ebd0c3971fa7b9a0
kamalika0363 <59131714+doesnots@users.noreply.github.com>
2023-09-18 13:07:17 +0530

    Register Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import RegisterSection from './components/RegisterSection/registerSection.jsx';
+// import RegisterSection from './components/RegisterSection/registerSection.jsx';
 
+import RegisterSection from "./components/RegisterSection_doesnots/RegisterSection.jsx";
 function App() {
   // do not add css in this file
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        spline: ["Spline Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
