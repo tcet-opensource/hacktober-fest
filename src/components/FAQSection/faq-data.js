1a3886544f63ce12c27ac1361b285b420a058a03
kamalika0363 <59131714+doesnots@users.noreply.github.com>
2023-09-27 13:46:39 +0530

    FAQ added

--- src/components/FAQSection/faq-data.js
+++ src/components/FAQSection/faq-data.js
@@ -1,26 +1,32 @@
 export default [
   {
     id: 1,
-    question: "Can I participate as an individual or in a team?",
+    question: "Will I receive a reward for participating in Hacktoberfest?",
     answer:
-      "This information will be provided during the registration process.",
+      "Absolutely! By participating, you can earn a digital reward kit courtesy of DigitalOcean and our sponsors and partners. Once you have completed four accepted pull/merge requests, your digital reward will be on its way to you.",
   },
   {
     id: 2,
-    question: "Can I participate as an individual or in a team?",
+    question: "How will I receive my digital reward?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius erat vitae ipsum fermentum aliquet. Aliquam erat volutpat. Praesent risus felis, auctor id commodo in, sagittis nec nunc. ",
+      "You'll be notified in your Hacktoberfest profile on the Hacktoberfest website when your digital reward is ready. Additionally, you'll receive an email from Holopin with instructions on how to claim your reward.",
   },
   {
     id: 3,
-    question: "Can I participate as an individual or in a team?",
+    question: "I signed up for Hacktoberfest in mid-October. Will the pull/merge requests I made earlier in October count?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius erat vitae ipsum fermentum aliquet. Aliquam erat volutpat. Praesent risus felis, auctor id commodo in, sagittis nec nunc. ",
+      "Yes, they will! All pull/merge requests created between October 1 and October 31 will count towards your total, regardless of when you registered for Hacktoberfest.",
   },
   {
     id: 4,
-    question: "Can I participate as an individual or in a team?",
+    question: "Do issues or commits count towards my total?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut varius erat vitae ipsum fermentum aliquet. Aliquam erat volutpat. Praesent risus felis, auctor id commodo in, sagittis nec nunc. ",
+      "No, only pull/merge requests count towards the total.",
+  },
+  {
+    id: 5,
+    question: "Do multiple pull/merge requests to the same repository count?",
+    answer:
+      "Yes, they do! Each pull/merge request is counted separately, so feel free to contribute as much as you can to the same repository.",
   },
 ];
--- src/components/RegisterSection/index.jsx
+++ src/components/RegisterSection/index.jsx
@@ -1,29 +1,28 @@
 export default function RegisterSection() {
-    return (
-        <div className="px-8 md:px-16">
-
-            <div
-                className="relative h-[calc(2*100vh/3)] md:h-[calc(100vh-100px)] lg:h-[calc(100vh-20px)] bg-no-repeat bg-cover bg-center bg-[#15122a] border border-[#3C3C3C] rounded-2xl flex flex-col items-center overflow-hidden"
-                style={{
-                    backgroundImage: "url('RegisterSection/register.svg')",
-                }}
-            >
-                <div className="flex flex-col items-center justify-between mt-8 md:mt-16 [&>*]:text-[#E0E2FF]">
-                    <h2 className="text-3xl font-medium sm:text-3xl text-center md:text-5xl font-spline">
-                        Find out about
-                        <br />
-                        Hacktober Fest
-                    </h2>
-                    <p className="justify-center mt-3 px-4 text-center text-md sm:text-base md:text-xl">
-                        Lorem ipsum dolor sit amet consectetur.
-                        <br />
-                        Tellus cringissim amet ultricies lobortis nunc egestas amet id.
-                    </p>
-                    <button className="bg-[#4C58FE] font-medium text-white px-3 sm:px-3 md:px-4 py-2 sm:py-1.5 md:py-2 rounded-md mt-8">
-                        Register Now
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="px-8 md:px-16">
+      <div
+        className="relative h-[calc(2*100vh/3)] md:h-[calc(100vh-100px)] lg:h-[calc(100vh-20px)] bg-no-repeat bg-cover bg-center bg-[#15122a] border border-[#3C3C3C] rounded-2xl flex flex-col items-center overflow-hidden"
+        style={{
+          backgroundImage: "url('RegisterSection/register.svg')",
+        }}
+      >
+        <div className="flex flex-col items-center justify-between mt-8 md:mt-16 [&>*]:text-[#E0E2FF]">
+          <h2 className="text-3xl font-medium sm:text-3xl text-center md:text-5xl font-spline">
+            Find out about
+            <br />
+            Hacktober Fest
+          </h2>
+          <p className="justify-center mt-3 px-4 text-center text-md sm:text-base md:text-xl">
+            Lorem ipsum dolor sit amet consectetur.
+            <br />
+            Tellus cringissim amet ultricies lobortis nunc egestas amet id.
+          </p>
+          <button className="bg-[#4C58FE] font-medium text-white px-3 sm:px-3 md:px-4 py-2 sm:py-1.5 md:py-2 rounded-md mt-8">
+            Register Now
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
