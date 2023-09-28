e747f6cf157a16fa3a7242f8a56292a4aa250f39
Shlok Prajapati <shlokprajapati2003@gmail.com>
2023-09-29 00:33:51 +0530

    Added Partners section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,46 +1,52 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import logo from "/Navbar/logo.png";
 import menu from "/Navbar/menu.svg";
 import discord from "/Navbar/discord.svg";
 import close1 from "/Navbar/close1.svg";
 
+const navLink = [
+  {
+    id: 0,
+    header: "Home",
+    link: "/",
+  },
+  {
+    id: 1,
+    header: "Docs",
+    link: "https://opensource.tcetmumbai.in/docs/about-tcetopensource",
+  },
+  {
+    id: 2,
+    header: "Achievements",
+    link: "https://opensource.tcetmumbai.in/",
+  },
+  {
+    id: 3,
+    header: "Projects",
+    link: "https://opensource.tcetmumbai.in/docs/category/projects",
+  },
+  {
+    id: 4,
+    header: "Team",
+    link: "https://github.com/orgs/tcet-opensource/teams",
+  },
+];
+
 export default function Navbar() {
   const [toggle, setToggle] = useState(false);
 
   const handleClick = () => setToggle((prevToggle) => !prevToggle);
-
   const closeMenu = () => setToggle(false);
 
-  const navLink = [
-    {
-      id: 0,
-      header: "Home",
-      link: "/",
-    },
-    {
-      id: 1,
-      header: "Docs",
-      link: "https://opensource.tcetmumbai.in/docs/about-tcetopensource",
-    },
-    {
-      id: 2,
-      header: "Achievements",
-      link: "https://opensource.tcetmumbai.in/",
-    },
-    {
-      id: 3,
-      header: "Projects",
-      link: "https://opensource.tcetmumbai.in/docs/category/projects",
-    },
-    {
-      id: 4,
-      header: "Team",
-      link: "https://github.com/orgs/tcet-opensource/teams",
-    },
-  ];
+  const handleScroll = () => {
+    const section = document.getElementById("sponserSection");
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
-    <div className="font-sans text-[#AFAFAF] mt-2">
+    <div className="font-sans text-[#451c1c] mt-2">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 px-2 sm:px-8">
           {/* Logo */}
@@ -58,13 +64,22 @@ export default function Navbar() {
               {navLink.map((step) => (
                 <a
                   key={step.id}
+                  id={step.id}
                   className="text-[#E4E7EC] hover:text-[#8369A8] px-2 py-1 rounded-md text-md font-medium"
                   href={step.link}
                   target="_blank"
+                  rel="noreferrer"
                 >
                   {step.header}
                 </a>
               ))}
+
+              <a
+                onClick={handleScroll}
+                className="text-[#E4E7EC] hover:text-[#8369A8] px-2 py-1 rounded-md text-md font-medium cursor-pointer"
+              >
+                Partners
+              </a>
             </div>
           </div>
 
@@ -72,11 +87,23 @@ export default function Navbar() {
             <button className="rounded-xl px-4 py-2 border-2 border-gray-500 text-lg">
               <div className="flex gap-1.5 text-white">
                 <img src={discord} alt="" />
-                <a href="https://discord.com/invite/r7ZhAREg2M" target="_blank">Discord</a>
+                <a
+                  href="https://discord.com/invite/r7ZhAREg2M"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Discord
+                </a>
               </div>
             </button>
             <button className="text-lg px-4 py-2 text-white rounded-lg mt-0 bg-[#4C58FE]">
-              <a href="https://events.mlh.io/events/10259-hacktoberfest-2023-in-mumbai" target="_blank">Register Now</a>
+              <a
+                href="https://events.mlh.io/events/10259-hacktoberfest-2023-in-mumbai"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Register Now
+              </a>
             </button>
           </div>
 
@@ -107,12 +134,19 @@ export default function Navbar() {
                 className="text-center cursor-pointer text-gray-300 hover:text-[#8B70AD] block px-3 py-2 rounded-md text-base font-medium"
                 href={step.link}
                 target="_blank"
+                rel="noreferrer"
                 onClick={closeMenu}
               >
                 {step.header}
               </a>
             ))}
-          
+
+            <a
+              onClick={handleScroll}
+              className="text-center cursor-pointer text-gray-300 hover:text-[#8B70AD] block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Partners
+            </a>
           </div>
         </div>
       )}
