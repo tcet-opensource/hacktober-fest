87bb1013fe7d032d576e6e8c76572426a9053ef3
Vishal Rawat <rawatvishal1707@gmail.com>
2023-09-30 17:26:34 +0530

    [FIX] Removed the glorious invisible gradient

--- src/components/PartnerSection/SponsoredBy.jsx
+++ src/components/PartnerSection/SponsoredBy.jsx
@@ -4,7 +4,7 @@ import sponsorGradient from "/PartnersSection/sponsorGradient.svg";
 function SponsoredBy() {
   return (
     <section className="">
-      <img src={sponsorGradient} className="absolute w-1/3 h-5/6" />
+      {/* <img src={sponsorGradient} className="absolute w-1/3 h-5/6" /> */}
       <div className="px-6 py-9 md:p-16">
         {SponArr.map((data, index) => (
           <div className="text-[#D6D9F6] md:mb-16 mb-12 items-center justify-center flex" key={index}>
